b089725a57842cb1dda44e09fe89aa847e70bfad
Juuso Piikkilä <juuso@meiko.fi>
2022-12-06 15:53:45 +0200

    day 2

---
+++ 2/index.ts
@@ -0,0 +1,121 @@
+import { strictEqual } from 'assert';
+import { getInput } from '../utils';
+
+type ItemName = 'rock' | 'paper' | 'scissors';
+
+interface Item {
+    name: ItemName
+    plays: string[]
+    score: number
+    answer: ItemName
+}
+
+const items: Item[] = [
+    {
+        name: 'rock',
+        plays: ['A', 'X'],
+        score: 1,
+        answer: 'paper',
+    },
+    {
+        name: 'paper',
+        plays: ['B', 'Y'],
+        score: 2,
+        answer: 'scissors',
+    },
+    {
+        name: 'scissors',
+        plays: ['C', 'Z'],
+        score: 3,
+        answer: 'rock',
+    },
+];
+
+function getScoreWithAnswers(data: string[]): number {
+    let score = 0;
+
+    for (const row of data) {
+        const [opponentPlay, elfPlay] = row.split(' ');
+
+        const opponentItem = items.find((item) => item.plays.includes(opponentPlay));
+        const elfItem = items.find((item) => item.plays.includes(elfPlay));
+
+        if (!opponentItem || !elfItem) {
+            throw new Error('Item not found');
+        }
+
+        score += elfItem.score;
+
+        if (opponentItem.answer === elfItem.name) {
+            score += 6;
+        } else if (opponentItem.name === elfItem.name) {
+            score += 3;
+        }
+    }
+
+    return score;
+}
+
+function getScoreWithOutcomes(data: string[]): number {
+    let score = 0;
+
+    for (const row of data) {
+        const [opponentPlay, outcome] = row.split(' ');
+
+        const opponentItem = items.find((item) => item.plays.includes(opponentPlay));
+
+        if (!opponentItem) {
+            throw new Error('Item not found');
+        }
+
+        let elfItem: Item | undefined;
+
+        if (outcome === 'X') {
+            // lose
+            elfItem = items.find((item) => item.name !== opponentItem.name && item.name !== opponentItem.answer);
+        } else if (outcome === 'Y') {
+            // draw
+            elfItem = opponentItem;
+            score += 3;
+        } else if (outcome === 'Z') {
+            // win
+            elfItem = items.find((item) => item.name === opponentItem.answer);
+            score += 6;
+        }
+
+        if (!elfItem) {
+            throw new Error('Item not found');
+        }
+
+        score += elfItem.score;
+    }
+
+    return score;
+}
+
+function part1(data: string[]): number {
+    return getScoreWithAnswers(data);
+}
+
+function part2(data: string[]): number {
+    return getScoreWithOutcomes(data);
+}
+
+async function main() {
+    const data = await getInput(__dirname);
+    const testData = [
+        'A Y',
+        'B X',
+        'C Z',
+    ];
+
+    strictEqual(getScoreWithAnswers(testData), 15);
+
+    console.log('Part 1', part1(data));
+
+    strictEqual(getScoreWithOutcomes(testData), 12);
+
+    console.log('Part 2', part2(data));
+}
+
+main();
