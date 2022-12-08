3ec7489bd24d84af8f3000efdc09a3b83f923576
Juuso Piikkilä <juuso@meiko.fi>
2022-12-08 09:40:31 +0200

    update template

--- _template/index.ts
+++ _template/index.ts
@@ -1,5 +1,5 @@
 import { strictEqual } from 'assert';
-import { getInput } from '../utils';
+import { getExampleInput, getInput } from '../utils';
 
 function part1(data: string[]): number {
     return 0;
@@ -11,15 +11,13 @@ function part2(data: string[]): number {
 
 async function main() {
     const data = await getInput(__dirname);
-    const testData = [
+    const testData = await getExampleInput(__dirname);
 
-    ];
-
-    // strictEqual(test(12), 2);
+    // strictEqual(test(testData), 2);
 
     console.log('Part 1', part1(data));
 
-    // strictEqual(test(14), 2);
+    // strictEqual(test(testData), 2);
 
     console.log('Part 2', part2(data));
 }
